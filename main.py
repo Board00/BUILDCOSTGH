e71ee2be4a64d105422d3abf9bef490e32d6a4ea
Board00 <konikakraba0@gmail.com>
2026-09-11 11:49:02 +0000

    updated added for admin well

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from routers import router as backend_router
 from frontend_routers import router as frontend_router
 from migration_router import router as migration_router
 from init_db import init_db   # 👈 import your init_db
+from database import SessionLocal
+from models import User
+from auth import hash_password
 
 app = FastAPI(
     title="FastAPI Backend by Koni",
@@ -25,3 +28,21 @@ register_error_handlers(app)
 @app.on_event("startup")
 def on_startup():
     init_db()   # 👈 runs table creation at startup
+
+@app.on_event("startup")
+def seed_admin():
+    db = SessionLocal()
+    admin = db.query(User).filter(User.username == "BUILDCOSTGH").first()
+    if not admin:
+        new_admin = User(
+            username="BUILDCOSTGH",
+            hashed_password=hash_password("BUILDCOSTGH"),  # hashed password
+            is_admin=True,
+            is_active=True
+        )
+        db.add(new_admin)
+        db.commit()
+        print("✅ Admin user created in Render DB")
+    else:
+        print("ℹ️ Admin already exists")
+    db.close()
